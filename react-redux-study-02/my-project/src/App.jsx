4aef70463f1ca5449c528c64d4513d0a7aa9da79
chartdesign <chartshorne@gmail.com>
2023-03-06 12:20:10 -0800

    adding the React study

---
+++ react-redux-study-02/my-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+import Counter from "./features/counter/Counter";
+
+function App() {
+  return (
+    <>
+      Hello Vite!
+      <Counter />
+    </>
+  );
+}
+
+export default App;
