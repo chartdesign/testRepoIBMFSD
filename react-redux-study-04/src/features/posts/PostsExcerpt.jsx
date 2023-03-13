efd15b5a2b8e3de1c5f43513793a428994a61567
chartdesign <chartshorne@gmail.com>
2023-03-13 10:30:11 -0700

    optimized react code

--- react-redux-study-04/src/features/posts/PostsExcerpt.jsx
+++ react-redux-study-04/src/features/posts/PostsExcerpt.jsx
@@ -3,16 +3,18 @@ import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
 import { Link } from "react-router-dom";
 
-const PostsExcerpt = ({ post }) => {
-  return (
-    <article className='border border-black p-4 m-2 rounded-2xl'>
-      <h3 className='font-semibold'>{post.title}</h3>
-      <p className='font-light'>{post.body.substring(0, 50)}...</p>
-      <p className='postCredit'>
-        <span className='font-bold mr-2 underline'>
-          <Link to={`post/${post.id}`}>View Post</Link>
-        </span>
+import { useSelector } from "react-redux";
+import { selectPostById } from "./postsSlice";
 
+const PostsExcerpt = ({ postId }) => {
+  const post = useSelector((state) => selectPostById(state, postId));
+
+  return (
+    <article>
+      <h2>{post.title}</h2>
+      <p className='excerpt'>{post.body.substring(0, 75)}...</p>
+      <p className='postCredit'>
+        <Link to={`post/${post.id}`}>View Post</Link>
         <PostAuthor userId={post.userId} />
         <TimeAgo timestamp={post.date} />
       </p>
@@ -20,4 +22,5 @@ const PostsExcerpt = ({ post }) => {
     </article>
   );
 };
+
 export default PostsExcerpt;
